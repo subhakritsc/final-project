80c8e185990ad4c64d0e5106dff655893cf33610
Subhakrit Chokchaitam <keenchokchaitam@gmail.com>
2024-04-13 17:07:50 +0700

    Update config.js

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,7 @@
 // TODO0: change the BACKEND_URL to your EC2 public IPv4
-export const BACKEND_URL = "http://localhost:3222";
+
+//export const BACKEND_URL = "http://localhost:3222";
+export const BACKEND_URL = "http://44.196.231.164:3222"
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
